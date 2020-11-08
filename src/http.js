984211c34723924b275cfc9a4c829665132fcd9c
ria <riatyagi28@gmail.com>
2020-11-08 13:17:29 +0530

    first commit

---
+++ src/http.js
@@ -0,0 +1,21 @@
+export function getData(url,callback){
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            const response = JSON.parse(xhr.responseText);
+            callback(response);
+        }
+    }
+    xhr.open('GET', url, true);
+    xhr.send(null);
+}
+
+export function getUrl(endpoint){
+    let host="";
+    if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+        host= "http://localhost:3000";
+    } else {
+       host = "http://52.66.129.248:3000";
+    }
+    return(process.env.REMOTE_BACKEND+"/"+endpoint);
+}
